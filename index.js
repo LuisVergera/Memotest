1ddbe5d3e5f5e582b9a74cbb7cc9bb0a27092833
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-04-27 16:11:49 -0300

    Se agregan imágenes de las cartas y función de iniciar juego

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
+const grid = document.querySelector(".grid");
+
 let round = 0;
 
+let cardDeck = [
+  {
+    name: "broly",
+    img: "images/broly.png",
+  },
+  {
+    name: "fusion",
+    img: "images/fusion.png",
+  },
+  {
+    name: "goku",
+    img: "images/goku.png",
+  },
+  {
+    name: "gohan",
+    img: "images/gohan.png",
+  },
+  {
+    name: "trunks",
+    img: "images/trunks.png",
+  },
+  {
+    name: "vegeta",
+    img: "images/vegeta.png",
+  },
+  {
+    name: "broly",
+    img: "images/broly.png",
+  },
+  {
+    name: "fusion",
+    img: "images/fusion.png",
+  },
+  {
+    name: "goku",
+    img: "images/goku.png",
+  },
+  {
+    name: "gohan",
+    img: "images/gohan.png",
+  },
+  {
+    name: "trunks",
+    img: "images/trunks.png",
+  },
+  {
+    name: "vegeta",
+    img: "images/vegeta.png",
+  },
+];
+
 function beginGame() {
-  newRound();
+  //newRound();
+  createCardGrid();
+}
+beginGame();
+
+function createCardGrid() {
+  for (let i = 0; i < cardDeck.length; i++) {
+    let card = document.createElement("img");
+    card.setAttribute("src", "images/cards2.png");
+    card.setAttribute("data-id", i);
+    grid.appendChild(card);
+  }
 }
 
 function newRound() {
